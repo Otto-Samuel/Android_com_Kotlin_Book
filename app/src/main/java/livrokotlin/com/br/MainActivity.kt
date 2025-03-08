22c905f598eeac27ecdfe3768bc82de10e0e4199
Otto Samuel <ottosamuel01@gmail.com>
2025-03-08 00:17:47 -0300

    adicionando um TextView

--- app/src/main/java/livrokotlin/com/br/MainActivity.kt
+++ app/src/main/java/livrokotlin/com/br/MainActivity.kt
@@ -2,6 +2,7 @@ package livrokotlin.com.br
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 
 class MainActivity : Activity()
@@ -9,5 +10,11 @@ class MainActivity : Activity()
     // ❗ decorar isso
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
+
+        // definindo um Texto para ser exibido
+        val texto = TextView(this)
+        texto.text = "hello kotlin"
+
+        setContentView(texto)
     }
 }
